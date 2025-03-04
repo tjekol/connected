98af9264113820e4b4d95156c093d805dbd3e9a1
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2025-03-04 23:02:41 +0100

    photo component

--- frontend/src/con/service/photo/photo.service.ts
+++ frontend/src/con/service/photo/photo.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ok } from 'assert';
 import { Observable } from 'rxjs';
 import { env } from '../../../environments/environment';
 
